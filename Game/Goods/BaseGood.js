f207a01ea620bcc2c332f15c5193cac92c7a3b73
Wade Marshall <wmmnola@gmail.com>
2020-03-18 19:36:15 -0500

    Deleted previous good valuation system.

--- Game/Goods/BaseGood.js
+++ Game/Goods/BaseGood.js
@@ -3,63 +3,10 @@ const normal = rand.normal();
 
 
 class BaseGood {
-  constructor(id, name,len, wid, demandFn) {
+  constructor(id, name) {
     this.id = id;
     this.name = name;
     this.amnt = 0;
-    this.length = len;
-    this.width = wid;
-    console.log(demandFn)
-    if(demandFn) {
-      this.demand_function = demandFn;
-    }
-    else {
-      this.coefficent = Math.abs(rand.float(1,5)*normal());
-      this.demand_function = (t) => {
-        return t.baseValue*this.coefficent * t.populationPercentile;
-      }
-    }
-    this.AmntDemandGrid = Array(len).fill().map(a => Array(wid));
-    this.PriceGrid = Array(len).fill().map(a => Array(wid));
-    this.domainAmnts = [];
-  }
-  setDmnAmnt(n){
-    this.domainAmnts = Array(n).fill(0);
-  }
-  addAmnt(n){
-    this.amnt += n;
-  }
-  initializeDemand(board){
-    for(let x = 0; x < board.length; x++){
-      for(let y = 0; y < board.width; y++){
-        let d = 0;
-        let p = 0;
-        let tile = board.grid[x][y];
-        if(!tile.water) {
-          d = this.demand_function(tile);
-          p = d/tile.populationPercentile;
-        }
-        this.AmntDemandGrid[x][y] = d;
-        this.PriceGrid[x][y] =p;
-      }
-    }
-    this.calculateAvgPrice();
-}
-  calculateAvgPrice(){
-    let sum = 0;
-    let count = 0;
-    for(let i = 0; i < this.length; i++){
-      for(let j = 0; j < this.width; j++) {
-        if(this.AmntDemandGrid[i][j] > 0){
-        sum += this.PriceGrid[i][j];
-        count += 1
-      }
-      }
-    }
-    console.log("Average price of "+this.name +":"+sum/count)
-  }
-  getDemand(tile){
-    return this.AmntDemandGrid[tile.x][tile.y]
   }
 
 
--- Game/Goods/PrimaryGood.js
+++ Game/Goods/PrimaryGood.js
@@ -2,26 +2,12 @@ const BaseGood = require("./BaseGood")
 
 
 class PrimaryGood extends BaseGood {
-  constructor(id, name, color, len, wid, demandFn) {
+  constructor(id, name, color) {
     super(id, name, len, wid, demandFn);
     this.color = color;
     this.tiles = [];
   }
 
-
-  produce(domains) {
-    for(let tile of this.tiles){
-      let increase = tile.baseValue * 0.1;
-      this.addAmnt(increase);
-      if(tile.claimed) {
-        this.domainAmnts[tile.domainID] += increase;
-      }
-    }
-    console.log(this.domainAmnts)
-    for(let d of domains){
-      d.setResourceAmnt(this, this.domainAmnts[d.id]);
-    }
-  }
   addTile(t){
     this.tiles.push(t);
   }
